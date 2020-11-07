f99263b59055aac735b7128e69567558ed632eb8
thiampo <babacacthiam@gmail.com>
2020-11-07 21:50:27 +0000

    seconde exo

---
+++ exo1a.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+    int a ;
+    
+    printf("Veuillez saisir un nombre compris entre 1 et 10\n");
+    scanf("%d\n",&a);
+    while((a>=1) && (a<=10) ){
+
+        printf("%d\n",a) ;
+
+       
+         }
+
+
+
+    
+    
+    
+    return 0
+    ;
+}
